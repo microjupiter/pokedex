7811ec657162357113e7aace4d24fa248b178be5
Kyle Wilson <kyle@hinterlandstills.com>
2021-01-04 13:00:48 -0600

    trainers table creation

--- db/migrate/20210104183906_create_trainers.rb
+++ db/migrate/20210104183906_create_trainers.rb
@@ -1,6 +1,11 @@
 class CreateTrainers < ActiveRecord::Migration
   def change
     create_table :trainers do |t|
+      t.string :name
+      t.integer :age
+      t.string :hometown
+      t.string :username
+      t.string :password_digest
 
       t.timestamps null: false
     end
