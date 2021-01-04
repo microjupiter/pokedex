c6ad08ca3ccc7ce14db07b2dde703fc0c837b2a7
Kyle Wilson <kyle@hinterlandstills.com>
2021-01-04 16:20:50 -0600

    dyanmic welcome message on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,14 +14,38 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do 
-    
-    @trainer = Trainer.create(username: params["username"], password: params["password"])
-    session[:id] = @trainer.id
+   
+    @trainer = Trainer.create(username: params["username"], password_digest: params["password"])
+    session[:user_id] = @trainer.id
     redirect '/home'
   end
 
+  # post '/sessions' do
+  #   @trainer = Trainer.find_by(username: params[:username], password: params[:password])
+  #   if @trainer
+  #     session[:id] = @trainer.id
+  #     redirect '/home'
+  #   end
+  #   redirect '/signup'
+  #   end
+
   get '/home' do
+    @trainer = Trainer.find_by_id(session[:user_id])
+    if logged_in?
     erb :home
+    else
+      redirect '/'
+    end
   end
+
+  helpers do
+  def current_user
+    @current_user ||= Trainer.find_by_id(session[:user_id])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+end
 
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,5 +1,6 @@
 class Trainer < ActiveRecord::Base
   has_secure_password
+  validates_presence_of :username, :password_digest
   has_many :pokedexes
   has_many :pokemon
 end
