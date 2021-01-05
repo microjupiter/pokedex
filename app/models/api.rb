9cba38080deaac7249afdab3bc2594da21902b59
Kyle Wilson <kyle@hinterlandstills.com>
2021-01-05 10:35:10 -0600

    app/models/api: finished api to create pokemon objects and their attributes

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,6 +1,6 @@
 class Api
 
-  def self.pokemon_creator
+  def pokemon_creator
     url = "https://pokeapi.co/api/v2/pokemon?limit=151"
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
@@ -9,28 +9,13 @@ class Api
     @pokemon = @pokemon_data["results"].collect do |pokemon|
       pokemon
     end
-
-
-
-    # def pokemon_new
-    #   self.each do |pokemon|
-    #     pokemon =  "http://pokeapi.co/api/v2/#{pokemon["name"]}"
-    #     poke_uri = URI.parse(pokemon)
-    #     response = Net:HTTP.get_response(poke_uri)
-    #     response.body
-    #     pokemon_data = JSON.parse(response.body)
-    #     Pokemon.new(name: pokemon_data["name"], type: pokemon_data["types"][0]["type"]["name"])
-    #   end
-    # end
-
-    @pokemon_data.each do |pokemon|
-      Pokemon.new(name: pokemon["name"], type: pokemon["url"])
+    @pokemon.each do |pokemon|
+      pokemons = "https://pokeapi.co/api/v2/pokemon/#{pokemon["name"]}"
+      poke_uri = URI.parse(pokemons)
+      responses = Net::HTTP.get_response(poke_uri)
+      responses.body
+      pokemon_data = JSON.parse(responses.body)
+      Pokemon.create(name: pokemon_data["name"]) 
     end
   end
 end
-
-
-
-
-
-
